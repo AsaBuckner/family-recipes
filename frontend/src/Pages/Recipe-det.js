bfbfa6a013165068c083c237e9fc54c4a93fef76
Asa <asabuckner19@gmail.com>
2023-12-05 04:30:03 -0500

    Front end framework established

---
+++ frontend/src/Pages/Recipe-det.js
@@ -0,0 +1,32 @@
+import react from "react"
+
+export default function Home(){
+return(
+    <div>
+        <header>
+            <img/>
+            // * Login comp : profile comp
+            <h1>${"<Family>"} Recipes</h1>
+        </header>
+
+        <section className="Catergories">
+            //* Occasion Bar
+            //* Type Bar
+        </section>
+        
+        <section className="Title-Tags">
+            <h2>
+                //* Title
+            </h2>
+            <div>
+                //* Map Tags
+            </div>
+        </section>
+
+        <section>
+            //* Map Recipes
+        </section>
+
+    </div>
+)
+}
